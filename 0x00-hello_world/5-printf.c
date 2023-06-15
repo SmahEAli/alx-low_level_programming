c7bc8aefe2019e9a7563eda51ef5be6a8cb99faf
SmahEAli <samahelnourali@gmail.com>
2023-06-15 16:26:06 -0700

    t5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main -prints "somthing"
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a piece of art,\n");
+	return (0);
+}

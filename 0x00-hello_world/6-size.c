0baff2501e184ff2c2ade11989109ad03605f97f
SmahEAli <samahelnourali@gmail.com>
2023-06-15 17:18:52 -0700

    t6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	printf("Size of various types:\n");
 	printf("size of char: %zu byte(s)\n", sizeof(char));
 	printf("size of an int: %zu byte(s)\n", sizeof(int));
 	printf("size of a long int: %zu byte(s)\n", sizeof(long int));

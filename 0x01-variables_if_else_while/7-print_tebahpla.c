a2a11430a5da34a49e3261c84f3695b59bdcc967
SmahEAli <samahelnourali@gmail.com>
2023-06-16 18:13:27 -0700

    t7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints alphabet bel galaba
+ *
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	char _c;
+
+	for (_c = 'z'; _c <= 'a'; _c++)
+	{
+		putchar(_c);
+	}
+	putchar('\n');
+	return (0);
+}

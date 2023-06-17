e5d33d601f86567038d70417927d702ae5845106
SmahEAli <samahelnourali@gmail.com>
2023-06-16 17:00:50 -0700

    t3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,14 @@
 int main(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
+	putchar(c);
         }
 	for (c = 'A'; c <= 'Z'; c++)
         {
-                putchar(c);
+	putchar(c);
         }
-        putchar('\n');
-        return (0); }
+	putchar('\n');
+	return (0); }

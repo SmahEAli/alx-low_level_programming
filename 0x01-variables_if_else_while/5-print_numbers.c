c23bab0beea2a32e692f0fac1b797c066054f287
SmahEAli <samahelnourali@gmail.com>
2023-06-16 17:35:40 -0700

    t5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - prints "numbers" 0-9
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for(n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0); }

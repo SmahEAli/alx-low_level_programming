7c46f1ee04e83fa2c31d581474c3b6b82bd93d11
SmahEAli <samahelnourali@gmail.com>
2023-06-16 17:17:31 -0700

    t4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - prints the alphabetics
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+        char ch;
+       
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+        if (ch != 'e'&& ch != 'q')
+        putchar(ch);
+        }
+        putchar('\n');
+        return (0); }

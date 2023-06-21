0892f28ada9371f6c856d80de6462e24204fe6ea
SmahEAli <samahelnourali@gmail.com>
2023-06-20 22:21:25 -0700

    t11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,13 +18,13 @@ void print_to_98(int n)
 				printf("\n");
 				break;
 			}
-			else 
+			else
 			{
 				printf("%d, ", n);
 			}
 		}
 	}
-	else 
+	else
 	{
 		for (; n >= 98; n--)
 		{
@@ -34,7 +34,7 @@ void print_to_98(int n)
 				printf("\n");
 				break;
 			}
-			else 
+			else
 			{
 				printf("%d, ", n);
 			}

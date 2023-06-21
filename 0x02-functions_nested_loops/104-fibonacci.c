bbeaf1e33baae66866c80c3870af001135ad157b
SmahEAli <samahelnourali@gmail.com>
2023-06-21 12:05:42 -0700

    advanced t16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,7 +8,9 @@
 int main(void)
 {
 	int  ct;
-	unsigned long f1 = 0, f2 = 1, sum, f1_h1, f1_h2, f2_h1, f2_h2, h1, h2;
+	unsigned long f1 = 0, f2 = 1, sum;
+	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
+	unsigned long h1, h2;
 
 	for (ct = 0; ct < 92; ct++)
 	{

3237612a088242b0b4278b962f9d9432b46c582a
SmahEAli <samahelnourali@gmail.com>
2023-06-21 08:56:06 -0700

    advanced t15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of even value terms>4000000
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	long x = 1, y = 2, sum = y;
+
+	while(x + y < 4000000)
+	{
+		y = y + x;
+		if (y % 2 == 0)
+		{
+			sum = sum + y;
+		}
+		x = y - x;
+		++i;
+	}
+	printf("%ld\n", sum);	
+	return (0);
+}

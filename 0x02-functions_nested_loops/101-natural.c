78c7d404c550c813cd475470b0ae7abb9d71563c
SmahEAli <samahelnourali@gmail.com>
2023-06-21 06:33:18 -0700

    t13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include <stdio.h>
 
 /**
  * main - prints sim of multiple of 3,5 until 1024
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, x = 0;
 
-	while ()
+	while (i < 1024)
 	{
-		if ((i % 3 ==0) || (i%5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			x = x + i;
 		}

307b3be8deea32e2b650b2563dbd3156e3715beb
SmahEAli <samahelnourali@gmail.com>
2023-06-20 22:44:35 -0700

    advanced_t12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the time table of input n, starting with 0
+ * @n: the value of the xtable
+ * Return: n
+ */
+void print_times_table(int n)
+{
+	int num, mul, x;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mul = 1; mul <= n; mul++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				x = num * mul;
+				if (x <= 99)
+				{
+					_putchar(' ');
+				}
+				if (x <= 9)
+				{
+					_putchar(' ');
+				}
+				if (x >= 100)
+				{
+					_putchar((x / 100) + '0');
+					_putchar(((x / 10)) % 10  + '0');
+				}
+				else if (x <= 99 && x >= 10)
+				{
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+				_putchar('\n');
+			}
+		}
+	}
+}

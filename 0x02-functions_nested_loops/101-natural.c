fb01b42795e76f21557438f683ea7febb1cdfa74
SmahEAli <samahelnourali@gmail.com>
2023-06-21 06:01:15 -0700

    advanced t15

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * main - prints sim of multiple of 3,5 until 1024
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, x = 0;
+
+	while ()
+	{
+		if ((i % 3 ==0) || (i%5 == 0))
+		{
+			x = x + i;
+		}
+	i++;
+	}
+	printf("%d\n", x);
+	return (0);
+}

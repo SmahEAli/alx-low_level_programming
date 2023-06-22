c2897ca96c8e3f625ff6ba2b4552dda99afb341d
SmahEAli <samahelnourali@gmail.com>
2023-06-21 23:00:43 -0700

    t0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * main - tests function that prints if int is +ve or -ve
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
 	int i;

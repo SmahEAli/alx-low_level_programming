58789a776f259c3793f119c4009068240dddb341
SmahEAli <samahelnourali@gmail.com>
2023-06-23 18:15:49 -0700

    t10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,11 +19,11 @@ void print_triangle(int size)
 		{
 			for (y = size - x; y > 1; y--)
 			{
-				_putchar('32');
+				_putchar(32);
 			}
 			for (z = 0; z <= x; z++)
 			{
-				_putchar('35');
+				_putchar(35);
 			}
 			_putchar('\n');
 		}

0707b0f3b9dcc1bcf68ed7e2ae9f419498e59d51
SmahEAli <samahelnourali@gmail.com>
2023-06-29 09:42:28 -0700

    t0 p 0x06

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strncat - concstenstes 2 strings
- * @dest: the string to be appended on
- * @src: the string to be to dest
- * @n: the number of bytes of src
+ * _strcat - concstenstes 2 strings
+ * @dest: the destination string
+ * @src: the string source
  * Return: a pointer
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int i = 0, dest_len = 0;
+	int i, dest_len = 0;
 
-	while (dest[i++])
+	while (dest[dest_len])
 	{
 		dest_len++;
 	}
-	for (i = 0; src[i] && i < n; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
-		dest[dest_len++] = src[i];
+		dest[dest_len] = src[i];
+		dest_len++;
 	}
+	dest[dest_len] = '\0';
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concstenstes 2 strings
+ * @dest: the string to be appended on
+ * @src: the string to be to dest
+ * @n: the number of bytes of src
+ * Return: a pointer
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
+	{
+		dest_len++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[dest_len++] = src[i];
+	}
+	return (dest);
+}

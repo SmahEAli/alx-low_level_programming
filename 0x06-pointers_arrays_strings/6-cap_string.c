340f9674c94891bf06dc4c0e5938aba7b67735ac
SmahEAli <samahelnourali@gmail.com>
2023-06-29 19:39:34 -0700

    t6 p:0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,33 +5,33 @@
  * @st: the string to be capitalized
  * Return: a pointer
  */
-char *cap_string(char *st)
+char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (st[i])
+	while (str[i])
 	{
-		while (!(st[i] >= 'a' && st[i] <= 'z)')
+		while (!(str[i] >= 'a' && str[i] <= 'z)')
 		{
 			i++;
 		}
-		if (st[i - 1] == ' ' ||
-		st[i - 1] == '\t' ||
-		st[i - 1] == '\n' ||
-		st[i - 1] == ',' ||
-		st[i - 1] == ';' ||
-		st[i - 1] == '!' ||
-		st[i - 1] == '?' ||
-		st[i - 1] == '"' ||
-		st[i - 1] == '(' ||
-		st[i - 1] == ')' ||
-		st[i - 1] == '{' ||
-		st[i - 1] == '}' ||
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
 		i == 0)
 		{
-			st[i] -= 32;
+			str[i] -= 32;
 		}
 		i++;
 	}
-	return (st);
+	return (str);
 }

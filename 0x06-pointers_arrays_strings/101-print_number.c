6d353c42b280599eee5ecf0b52d9d5ead43e672b
SmahEAli <samahelnourali@gmail.com>
2023-06-30 07:41:39 -0700

    advanced t9 p:0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,14 @@
  */
 void print_number(int n)
 {
-	unsigned int n0;
+	unsigned int n0 = n;
 
-	n0 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n0 = -n;
+		n0 = -n0;
 	}
-	if (n0 / 10 != 0)
+	if ((n0 / 10) > 0)
 	{
 		print_number(n0 / 10);
 	}

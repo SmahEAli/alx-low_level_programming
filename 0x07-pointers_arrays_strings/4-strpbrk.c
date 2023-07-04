57f7fca328ac55a8b9b019a5ca6b61abb29209ff
SmahEAli <samahelnourali@gmail.com>
2023-07-03 19:41:12 -0700

    t4: 0x07

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * _strpbrk: search for string for any of set of bytes
- * @s: string s
- * @accept: string accept
- * Return: Always 0 returns pointer s or NULL
+ * _strpbrk - search for string for any of set of bytes
+ * @s: input string s
+ * @accept: input string accept
+ * Return: Always 0 returns pointer s or NULl
  */
 char *_strpbrk(char *s, char *accept)
 {

18c5b1f0f26d213b36150db35efb77786f332efd
SmahEAli <samahelnourali@gmail.com>
2023-07-05 15:13:49 -0700

    t6: 0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - returns if no. is prime
+ * @n: input number
+ * Return: Always 0(Success) int
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_number(n));
+}

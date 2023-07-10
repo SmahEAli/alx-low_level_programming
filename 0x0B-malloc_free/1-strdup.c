8498d29b965c1a79d6c2bbc22b6d45bf4604613f
SmahEAli <samahelnourali@gmail.com>
2023-07-10 09:26:37 -0700

    t1: p 0x0B-malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - string duplicated parameter
+ * @str: the string to copy
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	int c = 0, i = 1;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i])
+	{
+		i++;
+	}
+	s = malloc((sizeof(char) * i) + i);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	while (c < i)
+	{
+		s[c] = str[c];
+		c++;
+	}
+	s[c] = '\0';
+	return (s);
+}

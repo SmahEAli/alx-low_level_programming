d4227a967d52d34d40556738900edd316ff5824c
SmahEAli <samahelnourali@gmail.com>
2023-07-11 16:26:38 -0700

    t3: 0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the x of 2 argument no.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always (0)
+ */
+int main(int argc, char *argv[])
+{
+	int n = 0, n1 = 0;
+
+	if (argc == 3)
+	{
+		n = atoi(argv[1]);
+		n1 = atoi(argv[2]);
+		printf("%d\n", n * n1);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}

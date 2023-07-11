ed703de3a6e0d8bb3d11993384d3a29cf7a4c2b0
SmahEAli <samahelnourali@gmail.com>
2023-07-11 10:39:09 -0700

    advanced t6: 0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -53,4 +53,31 @@ char **strtow(char *str)
 	char **st;
 	int i = 0, ws, w, ls, l;
 
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	ws = count_words(str);
+	if (ws == 0)
+		return (NULL);
+	st = malloc(sizeof(char *) * (ws + 1));
+	if (st == NULL)
+		return (NULL);
+	for (w = 0; w < ws; w++)
+	{
+		while (str[i] == ' ')
+			i++;
+		ls = word_len(str + i);
+		st[w] = malloc(sizeod(char) * (ls + 1));
+		if (st[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(st[w]);
+			free(st);
+			return (NULL);
+		}
+		for (l = 0; l < ls; l++)
+			st[w][l] = str[i++];
+		st[w][l] = '\0';
+	}
+	st[w] = NULL;
+	return (st);
 }

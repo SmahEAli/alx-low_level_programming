0104f9c36b1e2d4735b8b0fc5988a15709b6daeb
SmahEAli <samahelnourali@gmail.com>
2023-08-04 15:27:13 -0700

    tasks 0-5 of 0x14-bit_manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int putchar(char c);
-int atoi(char *s);
+int _putchar(char c);
+int atoi(char *str);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);

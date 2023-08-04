8843995abab8d854fda266423aa0c7368c2f04f2
SmahEAli <samahelnourali@gmail.com>
2023-08-04 16:07:59 -0700

    Tasks 0-7 of 0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - fn for checking endianness
+ * Return: 0 for big or 1 for small
+ */
+int get_endianness(void)
+{
+	unsigned int e = 1;
+	char *c = (char*) & e;
+	return (*c);
+}

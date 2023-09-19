e5ee3466768b49b7f29b334da044df54f278afbd
SmahEAli <samahelnourali@gmail.com>
2023-09-18 18:53:48 -0700

    3rd commit T2 0x18.C

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds x to y
+ * @x:1st number
+ * @y: 2nd number
+ * Return: Always (success) 0
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - subractts y from x
+ * @x:1st number
+ * @y: 2nd number
+ * Return: Always (success) 0
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * mult - multiply x by y
+ * @x:1st number
+ * @y: 2nd number
+ * Return: Always (success) 0
+ */
+int mult(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * div - divide x by y
+ * @x:1st number
+ * @y: 2nd number
+ * Return: Always (success) 0
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+
+/**
+ * mod - remainder of x div y
+ * @x:1st number
+ * @y: 2nd number
+ * Return: Always (success) 0
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
